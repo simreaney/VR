fbf8bd4abc68a1f879b164af984f4dacf2f304ff
simreaney <sim.reaney@durham.ac.uk>
2026-03-12 16:22:30 +0000

    update Hamilton VR

--- Hamilton/skin_vr.js
+++ Hamilton/skin_vr.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - VR - Skin
 // Pano2VR 7.1.11/21010
 // Filename: feather_vr_SMR.ggsk
-// Generated 2026-03-12T11:22:41
+// Generated 2026-03-12T16:21:41
 
 function pano2vrVrSkin(player,base) {
 	player.addVariable('node_cloner_vr_hasUp', 2, false, { ignoreInState: 0  });
